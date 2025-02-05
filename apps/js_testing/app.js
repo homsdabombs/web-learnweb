4ac5e2affb1f2457f140d54d836b7669d2fcc6d2
plonp <40211693+plonp@users.noreply.github.com>
2025-02-06 02:56:36 +0800

    save

--- apps/js_testing/app.js
+++ apps/js_testing/app.js
@@ -14,3 +14,17 @@ arr.forEach( function(_element, index) {
 	_element.classList.add("hehes");
 	console.log("test : " + index);
 });
+
+
+// addEventListener("keydown", (a) => {console.log(a)});
+
+function scrollCheckStatus() {
+	// const element = document.querySelector(".container-slides");
+	const element = document.querySelector("html"); //const element = document.documentElement;
+	const yy = element.scrollTop; 
+	document.getElementById("demo").innerHTML = yy;
+	console.log(yy);
+}
+// document.addEventListener("scroll", function() {console.log(yy)});
+document.addEventListener("scroll", scrollCheckStatus);
+// setInterval(scrollCheckStatus, 1000);
